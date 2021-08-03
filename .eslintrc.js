91afa148db1339576f0cebad96a03e0427e11a3a
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2021-08-03 22:09:33 +0900

    🐛 Fix eslint didn't work properly

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,8 +6,6 @@ module.exports = {
   },
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    sourceType: 'module',
-    ecmaVersion: 2019, // Node.js 12の場合は2019、他のバージョンのNode.jsを利用している場合は場合は適宜変更する
     tsconfigRootDir: __dirname,
     project: ['./tsconfig.eslint.json']
   },
