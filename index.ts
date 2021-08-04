2100a0c005272c5d6e8acd80a2de9d048325000f
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2021-08-04 22:04:43 +0900

    🐛 Fix uploads folder was not registered

--- index.ts
+++ index.ts
@@ -19,8 +19,11 @@ const potato = new Sonolus(app, config.sonolusOptions)
 // Add static folder (for use with sonolus-server-landing)
 app.use('/', express.static(config.static))
 
-// Add static folder (for use with upload)
+// Add static folder (for use with sonolus-pack)
 app.use('/', express.static(config.packer))
+
+// Add static folder (for use with upload)
+app.use('/repository/', express.static(config.uploads))
 
 // Load packer folder (for use with sonolus-pack)
 try {
@@ -28,26 +31,6 @@ try {
 } catch (e) {
   console.log('Sonolus-packer db was not valid!')
 }
-
-// Receive file upload
-app.post('/upload', (req, res) => {
-  upload(req, res, function (err) {
-    // This upload handler needs mimetype and filename
-    // If request from python with just binary, it return success but don't save file.
-    if (err instanceof multer.MulterError) {
-      // A Multer error occurred when uploading.
-      console.log(err)
-      res.status(400)
-      res.send('Requested file was not valid.')
-    } else if (err) {
-      // An unknown error occurred when uploading.
-      res.send(err)
-      res.status(500)
-      res.send('Internal server error. Please try again.')
-    }
-    res.send('File saved.')
-  })
-})
 
 // Startup the server
 app.listen(config.port, () => {
