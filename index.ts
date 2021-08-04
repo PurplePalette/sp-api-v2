ec2c88b15d5594a5a7ce2b30b0d94dcb8b17688e
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2021-08-04 22:33:41 +0900

    🎨 Move upload endpoint to upload.ts

--- index.ts
+++ index.ts
@@ -1,9 +1,7 @@
 import express from 'express'
-import fs from 'fs'
-import multer from 'multer'
 import { Sonolus } from 'sonolus-express'
 import { config } from './config'
-import { upload } from './potato/upload'
+import { uploadRouter } from './potato/upload'
 
 /*
  * Sonolus-uploader-core-2 Main class
@@ -17,40 +15,24 @@ const app = express()
 // Inject sonolus-express
 const potato = new Sonolus(app, config.sonolusOptions)
 
+// Inject uploadRouter
+app.use('/', uploadRouter)
+
 // Add static folder (for use with sonolus-server-landing)
 app.use('/', express.static(config.static))
 
-// Load uploads folder (for use with sonolus-pack)
-if (fs.existsSync(config.uploads)) {
-  try {
-    potato.load(config.uploads)
-  } catch (e) {
-    console.log('Database was not valid...')
-  }
-} else {
-  console.log(`${config.uploads} folder was not exist.`)
-  fs.mkdirSync(config.uploads)
-}
+// Add static folder (for use with upload)
+app.use('/repository/', express.static(config.uploads))
 
-// Receive file upload
-app.post('/upload', (req, res) => {
-  upload(req, res, function (err) {
-    // This upload handler needs mimetype and filename
-    // If request from python with just binary, it return success but don't save file.
-    if (err instanceof multer.MulterError) {
-      // A Multer error occurred when uploading.
-      console.log(err)
-      res.status(400)
-      res.send('Requested file was not valid.')
-    } else if (err) {
-      // An unknown error occurred when uploading.
-      res.send(err)
-      res.status(500)
-      res.send('Internal server error. Please try again.')
-    }
-    res.send('File saved.')
-  })
-})
+// Add static folder (for use with sonolus-pack)
+app.use('/', express.static(config.packer))
+
+// Load uploads folder (for use with sonolus-pack)
+try {
+  potato.load(config.packer)
+} catch (e) {
+  console.log('Sonolus-packer db was not valid!')
+}
 
 // Startup the server
 app.listen(config.port, () => {
--- potato/upload.ts
+++ potato/upload.ts
@@ -1,3 +1,4 @@
+import express from 'express'
 import multer from 'multer'
 import path from 'path'
 import { config } from '../config'
@@ -41,3 +42,25 @@ export const upload = multer({
     fieldNameSize: 50
   },
 }).single('file')
+
+export const uploadRouter = express.Router()
+
+// Receive file upload
+uploadRouter.post('/upload', (req, res) => {
+  upload(req, res, function (err) {
+    // This upload handler needs mimetype and filename
+    // If request from python with just binary, it return success but don't save file.
+    if (err instanceof multer.MulterError) {
+      // A Multer error occurred when uploading.
+      console.log(err)
+      res.status(400)
+      res.send('Requested file was not valid.')
+    } else if (err) {
+      // An unknown error occurred when uploading.
+      res.send(err)
+      res.status(500)
+      res.send('Internal server error. Please try again.')
+    }
+    res.send('File saved.')
+  })
+})
