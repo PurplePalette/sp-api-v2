54c4357f2abca4cc7c93aa76f0d9b10d835dc031
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2021-08-10 03:27:40 +0900

    🏷️ Extend LevelInfo type / express Request type

--- types/express.d.ts
+++ types/express.d.ts
@@ -1,5 +1,6 @@
 declare namespace Express {
     export interface Request {
-       userId?: string
+        userId?: string
+        localize?: any
     }
 }
---
+++ types/sonolus-express.d.ts
@@ -0,0 +1,46 @@
+import { LevelInfo } from 'sonolus-express'
+
+declare module 'sonolus-express' {
+    interface LevelInfo extends LevelInfo {
+        /**
+        * 独自要素: 楽曲のジャンル
+        */
+        'genre': string;
+        /**
+         * 独自要素: 楽曲が全体公開かどうか
+         */
+        'public': boolean;
+        /**
+         * 独自要素: 譜面作成者のユーザーID
+         */
+        'userId': string;
+        /**
+         * 独自要素: 譜面内のノーツ数
+         */
+        'notes': number;
+        /**
+         * 独自要素: データを作成したエポックミリ秒(ソート用)
+         */
+        'createdTime': number;
+        /**
+         * 独自要素: データを更新したエポックミリ秒(ソート用)
+         */
+        'updatedTime': number;
+        /**
+         * 独自要素: サムネのハッシュ値(DBを兼ねるため)
+         */
+        'coverHash': string;
+        /**
+         * 独自要素: データのハッシュ(DBを兼ねるため)
+         */
+        'dataHash': string;
+        /**
+         * 独自要素: BGMのハッシュ(DBを兼ねるため)
+         */
+        'bgmHash': string;
+        /**
+         * 独自要素: プレイされた回数
+         */
+        'playCount': number;
+    }
+}
