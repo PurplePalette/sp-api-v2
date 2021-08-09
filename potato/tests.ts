6a1befb3a4b6597296f8d9fe953760a3d99a9fe6
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2021-08-10 03:29:46 +0900

    🎨 Refactored tests endpoint

--- potato/tests.ts
+++ potato/tests.ts
@@ -1,37 +1,75 @@
-import express from 'express'
-import { Sonolus, defaultListHandler, listRouteHandler, LevelInfo, toLevelItem } from 'sonolus-express'
-import CustomLevelInfo from '../types/level'
+import {
+  Sonolus,
+  defaultListHandler, listRouteHandler,
+  detailsRouteHandler,
+  LevelInfo, toLevelItem, toServerInfo,
+  LocalizationText,
+} from 'sonolus-express'
+import type { Request, Response } from 'express'
+import CustomUserInfo from '../types/user'
+
+function getTestingLevels (sonolus: Sonolus, req: Request, res: Response) : LevelInfo[] {
+  const users = req.app.locals.users as CustomUserInfo[]
+  const userIds = users.filter(u => u.testId === req.params.testId)
+  if (userIds.length === 0) {
+    res.status(404).json({ message: 'Specified test was not found' })
+    return []
+  }
+  const filteredLevels = sonolus.db.levels.filter(l => l.userId === userIds[0].userId)
+  return filteredLevels
+}
 
 /**
  * Express: add/edit/delete level handler
 */
-export function createTestsRouter(sonolus: Sonolus) : express.Router {
-  const testsRouter = express.Router()
+export function installTestsEndpoints(sonolus: Sonolus): void {
+  /* Server info */
+  sonolus.app.get('/tests/:testId/info', (req, res) => {
+    req.localize = (text: LocalizationText) => sonolus.localize(text, req.query.localization as string)
+    const filteredLevels = getTestingLevels(sonolus, req, res)
+    if (filteredLevels.length === 0) { return }
+    res.json(
+      toServerInfo(
+        {
+          levels: filteredLevels.slice(0, 5),
+          skins: [], backgrounds: [], effects: [], particles: [], engines: []
+        },
+        sonolus.db,
+        req.localize
+      )
+    )
+  })
 
-  testsRouter.get('/:testId/info', (req, res) => {
-    // eslint-disable-next-line
-    // req.localize = (text: any) => sonolus.localize(text, req.query.localization as string)
-    const userId = req.params.testId
-    const levels = sonolus.db.levels as CustomLevelInfo[]
-    const filteredLevels = levels.filter(l => l.userId === userId)
-    console.log(filteredLevels)
-    const testsLevelListHandler = (
-      sonolus: Sonolus,
-      keywords: string | undefined,
-      page: number
-    ): {
+  /* Level list */
+  sonolus.app.get('/tests/:testId/levels/list', (req, res, next) => {
+    (async () => {
+      req.localize = (text: LocalizationText) => sonolus.localize(text, req.query.localization as string)
+      const filteredLevels = getTestingLevels(sonolus, req, res)
+      if (filteredLevels.length === 0) { return }
+      const testsLevelListHandler = (
+        sonolus: Sonolus,
+        keywords: string | undefined,
+        page: number
+      ): {
         pageCount: number
         infos: LevelInfo[]
       } => {
-      return defaultListHandler(
-        sonolus.db.levels,
-        ['name', 'rating', 'title', 'artists', 'author', 'description'],
-        keywords,
-        page
-      )
-    }
-    const r: Promise<void> = listRouteHandler(sonolus, testsLevelListHandler, toLevelItem, req, res)
-    console.log(r)
+        return defaultListHandler(
+          filteredLevels,
+          ['name', 'rating', 'title', 'artists', 'author', 'description'],
+          keywords,
+          page
+        )
+      }
+      await listRouteHandler(sonolus, testsLevelListHandler, toLevelItem, req, res)
+    })().catch(next)
   })
-  return testsRouter
+
+  /* Get level */
+  sonolus.app.get('/tests/:testId/levels/:levelName', (req, res, next) => {
+    (async () => {
+      req.localize = (text: LocalizationText) => sonolus.localize(text, req.query.localization as string)
+      await detailsRouteHandler(sonolus, sonolus.levelDetailsHandler, toLevelItem, req, res)
+    })().catch(next)
+  })
 }
